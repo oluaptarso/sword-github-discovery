ea898b7c31d29554723615bace6812ba96023804
paulohyst <oluaptarsso@gmail.com>
2022-12-04 15:09:13 -0300

    feat(cypress): add component tests support

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,6 +1,6 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Authentication", () => {
   it("visits the app root url", () => {
     cy.visit("/");
     cy.contains("a", "APP");
